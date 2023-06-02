10be988128bf70d8a04b2e0758e3f0e23b8e8fdd
Martin Perina <mperina@redhat.com>
2023-06-02 10:36:51 +0200

    Mark current directory as safe during SRPM build
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- .automation/build-srpm.sh
+++ .automation/build-srpm.sh
@@ -3,6 +3,9 @@
 # Directory, where build artifacts will be stored, should be passed as the 1st parameter
 ARTIFACTS_DIR=${1:-exported-artifacts}
 
+# Mark current directory as safe for git to be able to parse git hash
+git config --global --add safe.directory $(pwd)
+
 # Prepare source archive
 [[ -d rpmbuild ]] || mkdir -p rpmbuild
 
